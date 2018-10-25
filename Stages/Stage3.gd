3266d70206bb6815f009e3ab64987c23cc825260
agustafson19 <40578471+agustafson19@users.noreply.github.com>
2018-10-24 21:08:50 -0500

    Made a tilemap
    Made a tilemap in Piskel and put it in cubo, using it for a second stage that I made

--- Stages/Stage3.gd
+++ Stages/Stage3.gd
@@ -7,7 +7,3 @@ func _on_Cubo_toggle_menu():
 	$InGameMenu.show()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	get_tree().paused = true
-
-func _on_Goal_body_entered(body):
-	if body.is_in_group("Cubo"):
-		get_tree().change_scene("res://Stages/Stage3.tscn")
