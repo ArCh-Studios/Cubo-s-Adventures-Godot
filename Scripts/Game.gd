19e14ad46e780f362e98620b7de4fc450874ff22
agustafson19 <40578471+agustafson19@users.noreply.github.com>
2018-11-13 18:11:40 -0600

    Finnished current system
    Finnished constructing current system wherein a single game node frees and loads other stage nodes which dictate what the level is.

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -7,19 +7,20 @@ var stage
 func _ready():
 	packed_stage = load("res://Stages/Stage1.tscn")
 	stage = packed_stage.instance()
-	stage.set_name("stage")
 	add_child(stage)
+	move_child(stage, 0)
 
 func _on_Goal_body_entered(body):
 	if body.is_in_group("Cubo"):
 		stage.queue_free()
 		stage_num = stage_num + 1
-		packed_stage = load(set_text("res://Stages/Stage" + str(stage_num) + ".tscn"))
+		packed_stage = load("res://Stages/Stage" + str(stage_num) + ".tscn")
 		stage = packed_stage.instance()
 		add_child(stage)
+		move_child(stage, 0)
 		
 
 func _on_Cubo_toggle_menu():
-	$stage.InGameMenu.show()
+	$InGameMenu.show()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	get_tree().paused = true
--- Sprites/Goal.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-signal _on_Goal_body_entered
-
-func _ready():
-	connect("_on_Goal_body_entered", get_tree().get_root().get_node("Game"), "_on_Goal_body_entered")
