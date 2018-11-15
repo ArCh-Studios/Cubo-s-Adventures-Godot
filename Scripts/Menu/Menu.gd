45e20cb9fc39c7ec03e9e469862a431c2da3a04d
agustafson19 <40578471+agustafson19@users.noreply.github.com>
2018-11-15 17:45:23 -0600

    Added Music
    Added Looping Music, re-named a button and took care of some issues.

--- Scripts/Menu/Menu.gd
+++ Scripts/Menu/Menu.gd
@@ -4,3 +4,4 @@ func _ready():
 	var save_file = File.new()
 	if not save_file.file_exists("user://savegame.save"):
 		$CenterContainer/ContinueButton.hide()
+		$CenterContainer/RestartButton.text = "New Game"
