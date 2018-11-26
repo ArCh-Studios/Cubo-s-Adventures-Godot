7af390e85d54589d890e714baa6fff01acaf898f
agustafson19 <argus19@icstudents.org>
2018-11-26 17:13:40 -0600

    Added spikes to the tile set

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -13,28 +13,6 @@ func _ready():
 	add_child(stage)
 	move_child(stage, 0)
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-
-func _on_Goal_body_entered(body):
-	if body.is_in_group("Cubo"):
-		stage.queue_free()
-		current_stage += 1
-		if current_stage > save_data["max_stage"]:
-			save_data["max_stage"] = current_stage
-			save_game()
-		packed_stage = load("res://Stages/Stage" + str(current_stage) + ".tscn")
-		stage = packed_stage.instance()
-		stage.name = "Stage"
-		add_child(stage)
-		move_child(stage, 0)
-
-func _on_Cubo_toggle_menu(camera):
-	$InGameMenu.margin_left = camera.x - 260
-	$InGameMenu.margin_right = camera.x + 260
-	$InGameMenu.margin_top = camera.y - 450
-	$InGameMenu.margin_bottom = camera.y + 450
-	$InGameMenu.show()
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	get_tree().paused = true
 	
 func save_game():
 	var save_game = File.new()
@@ -49,3 +27,25 @@ func load_game():
 	save_file.open("user://savegame.save", File.READ)
 	save_data = parse_json(save_file.get_as_text())
 	save_file.close()
+
+func _on_Cubo_toggle_menu(camera):
+	$InGameMenu.margin_left = camera.x - 260
+	$InGameMenu.margin_right = camera.x + 260
+	$InGameMenu.margin_top = camera.y - 450
+	$InGameMenu.margin_bottom = camera.y + 450
+	$InGameMenu.show()
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	get_tree().paused = true
+
+func _on_Goal_body_entered(body):
+	if body.is_in_group("Cubo"):
+		stage.queue_free()
+		current_stage += 1
+		if current_stage > save_data["max_stage"]:
+			save_data["max_stage"] = current_stage
+			save_game()
+		packed_stage = load("res://Stages/Stage" + str(current_stage) + ".tscn")
+		stage = packed_stage.instance()
+		stage.name = "Stage"
+		add_child(stage)
+		move_child(stage, 0)
