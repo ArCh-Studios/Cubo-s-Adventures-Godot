b2fff467e79069df9f3345812f2b4db211159622
agustafson19 <argus19@icstudents.org>
2019-04-01 16:09:55 -0500

    Fixed Doors

--- Scripts/Stage.gd
+++ Scripts/Stage.gd
@@ -3,19 +3,27 @@ extends Node
 func _on_white_key_body_entered(body):
 	if body.is_in_group("Cubo"):
 		$WhiteKey.queue_free()
-		$WhiteDoor/WhiteLock.queue_free()
+		var doors = get_tree().get_nodes_in_group("WhiteDoor")
+		for door in doors:
+			door.get_child(0).queue_free()
 
 func _on_red_key_body_entered(body):
 	if body.is_in_group("Cubo"):
 		$RedKey.queue_free()
-		$RedDoor/RedLock.queue_free()
+		var doors = get_tree().get_nodes_in_group("RedDoor")
+		for door in doors:
+			door.get_child(0).queue_free()
 
 func _on_green_key_body_entered(body):
 	if body.is_in_group("Cubo"):
 		$GreenKey.queue_free()
-		$GreenDoor/GreenLock.queue_free()
+		var doors = get_tree().get_nodes_in_group("GreenDoor")
+		for door in doors:
+			door.get_child(0).queue_free()
 
 func _on_blue_key_body_entered(body):
 	if body.is_in_group("Cubo"):
 		$BlueKey.queue_free()
-		$BlueDoor/BlueLock.queue_free()
+		var doors = get_tree().get_nodes_in_group("BlueDoor")
+		for door in doors:
+			door.get_child(0).queue_free()
